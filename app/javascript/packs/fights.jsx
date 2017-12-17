825df6cda0c325541ed5f9cdc3a5b5d33f1cc164
alexandersideris <alexcs02356@gmail.com>
2017-12-18 00:27:46 +0200

    Added Bootstrap Successfuly
    Added a different gem, react-bootstrap, it works like a charm! The bad thing is that it is unstable atm so the apis might change until they get to v1.0.0.

--- app/javascript/packs/fights.jsx
+++ app/javascript/packs/fights.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import 'bootstrap/dist/css/bootstrap.css';
-import { Button } from 'reactstrap';
 import ShowFight from './ShowFight';
 import NavigationBar from './NavigationBar';
 
