6a81ec1e587d37aac4070fbd511cfcbd96990fd2
alexandersideris <alexander.sideris.west@gmail.com>
2017-12-25 18:38:01 +0200

    Small change

--- app/javascript/packs/fights.jsx
+++ app/javascript/packs/fights.jsx
@@ -174,20 +174,7 @@ class Fights extends React.Component {
                   <div>
                     <h1 style={{minWidth: '1140px', fontSize: 35, textAlign: 'center', paddingBottom: 50, paddingTop: 50 }}>{title}</h1>
                     <div>
-                      {fights.map(function(fight){
-                        return(
-                          <ShowFight
-                            isMobile={isMobile}
-                            context = { context }
-                            current_user = {c_user}
-                            has_voted = { fight.has_voted }
-                            fight = { fight.id }
-                            fighter_one = { fighters.find(function(e){ return e.id == fight.fighter_one_id; })}
-                            fighter_two = { fighters.find(function(e){ return e.id == fight.fighter_two_id; })}
-                            upvotes = {fight.upvotes}>
-                          </ShowFight>
-                        );
-                      })}
+                      <h1>Hi</h1>
                     </div>
                   </div>
 
