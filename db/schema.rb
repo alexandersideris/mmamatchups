4a80e3e8cd0985e9e0f64f811a1359e0960d5742
alexandersideris <alexander.sideris.west@gmail.com>
2018-12-11 16:59:33 +0200

    Small changes

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20171225114126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "fighters", force: :cascade do |t|
     t.string   "name"
     t.string   "division"
@@ -70,12 +76,12 @@ ActiveRecord::Schema.define(version: 20171225114126) do
   add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
   add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
-  create_table "widgets", force: :cascade do |t|
+  create_table "widgets", id: :bigserial, force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
